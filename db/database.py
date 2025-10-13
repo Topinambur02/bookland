5e062e1eddffc059c03f71cd5e9a638c66a79051
Topinambur02 <alext0408@yandex.ru>
2025-10-13 16:50:30 +0300

    fix: delete poetry run alembic in docker compose and replace localhost to postgres

--- db/database.py
+++ db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:postgres@localhost:5432/bookland"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:postgres@postgres:5432/bookland"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
